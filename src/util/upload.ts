26a753656c8f8a7c9ff5da4172e0a63c94a219d7
Vincent Rubinetti <vince.rubinetti@gmail.com>
2025-04-21 12:18:16 -0400

    fix pdf upload

--- src/util/upload.ts
+++ src/util/upload.ts
@@ -1,7 +1,7 @@
 import { expose } from "comlink";
 import { extractRawText } from "mammoth";
 import * as pdfjs from "pdfjs-dist";
-import workerSrc from "pdfjs-dist/build/pdf.worker?worker&url";
+import workerSrc from "pdfjs-dist/build/pdf.worker?url";
 
 /** https://github.com/mozilla/pdf.js/issues/10478#issuecomment-2242664642 */
 pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
